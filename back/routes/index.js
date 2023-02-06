f58d2eff7958bc3d715dd6968d49695f9fe90668
cloudcoke <lms4296@gmail.com>
2023-02-06 17:49:02 +0900

    feat: routes/index.js 수정

--- back/routes/index.js
+++ back/routes/index.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const userRouter = require("../src/users/user.route");
 // const authRouter = require("../src/auths/auth.route");
-// const boardRouter = require("../src/boards/board.route");
+const boardRouter = require("../src/boards/board.route");
 
 router.use("/users", userRouter);
 // router.use("/auths", authRouter);
-// router.use("/boards", boardRouter);
+router.use("/boards", boardRouter);
 
 module.exports = router;
