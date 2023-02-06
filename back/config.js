00fcd01a0d92d757a91d592cc2278cc8e5486e15
sangbeom <hhsb2441@gmail.com>
2023-02-06 14:40:36 +0900

    feat: back jwt 작업

--- back/config.js
+++ back/config.js
@@ -1,15 +1,20 @@
 require("dotenv").config();
+const crypto = require("crypto");
+const JWT = require("./lib/jwt");
 const env = process.env;
+const salt = env.SALT;
 
 const config = {
+  port: env.PORT,
+  jwt: new JWT({ crypto, salt }),
   db: {
     development: {
       username: env.DB_USER || "foo",
-      password: env.DB_PASSWORD || "1234",
+      password: env.DB_PASSWORD || "1q2w3e4r!",
       port: env.DB_PORT || "1234",
       host: env.DB_HOST || "0.0.0.0",
-      database: env.DB_DATABASE || "foo",
-      dialect: env.DB_DIALECT || "mssql",
+      database: env.DB_DATABASE || "",
+      dialect: env.DB_DIALECT || "",
       define: {
         freezeTableName: true,
         timestamps: false,
