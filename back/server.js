7f4ad29fa24a310897f86c6b6ef4da2086ac5f8d
Jerry <gch02933@naver.com>
2023-02-08 20:40:10 +0900

    feat: git병합작업

--- back/server.js
+++ back/server.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 const app = require("./app");
 const { sequelize } = require("./models");
-const boardDumy = require("./dumy/board_dumy");
+const boardDumy = require("./dumy/Boards_dumy");
 
 app.listen(process.env.PORT, async () => {
-  await sequelize.sync({ force: true });
+  await sequelize.sync({ force: false });
 
   for (let i = 0; i < boardDumy.length; i++) {
     sequelize.models.Boards.create(boardDumy[i]);
