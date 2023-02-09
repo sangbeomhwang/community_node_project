e05b1a02c2c6fc3e4335f83999ce669b31599a46
cloudcoke <lms4296@gmail.com>
2023-02-09 12:59:49 +0900

    feat: board list 파일 리팩토링

---
+++ front/public/js/lib/template.js
@@ -0,0 +1,34 @@
+const boardListTemplate = ({ boardidx, nickname, register, title, hashtag, hit, like, comment }) => `
+ <li data-boardidx="${boardidx}">
+   <div class="li_user">
+     닉네임 ${nickname}
+     <div>
+     <span>등록일시</span>
+       <span>${register}</span>
+     </div>
+   </div>
+   <div class="li_sub"><a href="#">${title}</a></div>
+   <div class="li_Cate">
+     <a href="#">#Gitjub #Repository</a>
+     <div>
+       <div>
+         조회수<img
+           src="https://i.postimg.cc/XYD9XSTJ/ph-eye.png"
+         /><span>${hit}</span>
+       </div>
+       <div>
+         좋아요<img
+           src="https://i.postimg.cc/JnXFPQbN/Vector.png"
+         /><span>0</span>
+       </div>
+       <div>
+         댓글<img
+           src="https://i.postimg.cc/5ynS3Hk5/Vector-1.png"
+         /><span>0</span>
+       </div>
+     </div>
+   </div>
+ </li>
+ `;
+
+export { boardListTemplate };
