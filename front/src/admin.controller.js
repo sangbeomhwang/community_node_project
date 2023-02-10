02b4ee5db837713f1d4a6221fa7ea8c4b46c69ce
sangbeom <hhsb2441@gmail.com>
2023-02-10 17:43:12 +0900

    feat: admin page layput 및 front router 작업

---
+++ front/src/admin.controller.js
@@ -0,0 +1,70 @@
+// const axios = require("axios");
+// const request = axios.create({
+//   baseURL: "http://127.0.0.1:3000",
+//   withCredentials: true,
+// });
+
+class AdminController {
+  async getIndex(req, res, next) {
+    try {
+      // console.log("admin 전달 확인용 : ", req.user);
+      const { userid, nickname, image, level } = req.user;
+      res.render("admin/admin_index.html", {
+        userid,
+        nickname,
+        image,
+        level,
+      });
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async getUsers(req, res, next) {
+    try {
+      const { userid, nickname, image, level } = req.user;
+      res.render("admin/admin_user.html", {
+        userid,
+        nickname,
+        image,
+        level,
+      });
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async getBoards(req, res, next) {
+    try {
+      const { userid, nickname, image, level } = req.user;
+      res.render("admin/admin_board.html", {
+        userid,
+        nickname,
+        image,
+        level,
+      });
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async getComments(req, res, next) {
+    try {
+      const { userid, nickname, image, level } = req.user;
+      res.render("admin/admin_comment.html", {
+        userid,
+        nickname,
+        image,
+        level,
+      });
+    } catch (e) {
+      next(e);
+    }
+  }
+}
+
+const adminController = new AdminController();
+
+module.exports = {
+  adminController,
+};
