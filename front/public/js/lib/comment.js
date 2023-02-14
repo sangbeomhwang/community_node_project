c621ea1839ff73b4e362d72b888e40fb15f30f4e
Jerry <gch02933@naver.com>
2023-02-14 13:44:37 +0900

    fix: comments CR 수정

--- front/public/js/lib/comment.js
+++ front/public/js/lib/comment.js
@@ -3,7 +3,7 @@ import request from '/js/lib/request.js'
 const template = ({ boardidx, content, nickname, register, commentidx, image }) => `
 <div id="depth_b" data-commentidx="${commentidx}">
 <div id="depth_info">
-    <img src="https://i.postimg.cc/qv5KM7Fv/Vector-1.png" alt="modify_img" />
+    <img src="https://i.postimg.cc/qv5KM7Fv/Vector-1.png">
     <div>
         <div>
             <img src="https://i.postimg.cc/qv5KM7Fv/Vector-1.png"><span id='depth_nic'>${nickname}</span>
@@ -15,45 +15,47 @@ const template = ({ boardidx, content, nickname, register, commentidx, image }) 
 </div>
 <textarea>${content}</textarea>
 <div id="btn">
-    <div id="depth_post">댓글쓰기</div>
+    <div id="depth_delete">삭제하기</div>
+    <div id="depth_put">수정하기</div>
 </div>
 </div>
  `;
 
 
 const commentBox = document.querySelector("#comment_depth")
-console.log(commentBox)
+// console.log(commentBox)
 
-// const render = async ({ boardidx }) => {
-//     console.log("boardidx ::::",boardidx)
-//     const response = await request.get(`/comments?boardidx=${boardidx}`)
-//     console.log('Commentdata :::',response.data)
+const render = async ({ boardidx }) => {
+    console.log("boardidx ::::",boardidx)
+    const response = await request.get(`/comments?boardidx=${boardidx}`)
+    console.log('Commentdata :::',response.data)
 
-//     console.log(commentBox)
-//     commentBox.innerHTML = ''
-//     for (let i = 0; i < response.data.length; i++) {
-//         console.log("################",response.data)
-//         commentBox.innerHTML += template(response.data[i])
-//     }
-//     return response
-// }
+    // console.log(commentBox)
+    commentBox.innerHTML = ''
+    for (let i = 0; i < response.data.length; i++) {
+        // console.log("################",response.data)
+        commentBox.innerHTML += template(response.data[i])
+    }
+    return response
+}
 
-// const boardidx = location.href.split('/')
-// // console.log(boardidx)
-// render({boardidx:boardidx[boardidx.length-1]})
+const boardidx = location.href.split('/')
+// console.log(boardidx)
+render({boardidx:boardidx[boardidx.length-1]})
 
 
 document.querySelector("#depth_post").addEventListener('click', async (e) => {
     e.preventDefault()
 
-    console.log(boardidx.value)
+    console.log(boardidx)
+    console.log(boardidx[4])
     console.log("nickname :::::::: ",nickname.innerHTML)
     
     const content = document.querySelector('textarea').value
     console.log("content ::: ", content)
 
     const data = {
-        boardidx: boardidx.value,
+        boardidx: boardidx[4],
         nickname: nickname.innerHTML,
         content,
     }
