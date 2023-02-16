3f60682deb0d785a112f965eb270b67059ca8205
cloudcoke <lms4296@gmail.com>
2023-02-16 15:17:21 +0900

    feat: 토큰 내용 변경

--- front/config.js
+++ front/config.js
@@ -5,7 +5,6 @@ const config = {
     host: process.env.KAKAO_HOST || "",
     rest_api_key: process.env.REST_API_KEY || "",
     redirect_uri: process.env.REDIRECT_URI || "",
-    client_secret: process.env.CLIENT_SECRET || "",
   },
 };
 
